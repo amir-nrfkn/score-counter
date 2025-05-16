3806751026c9090924e8b950e86595df59ffd18d
Amir Noorafkan <amir@BigMac.local>
2025-05-16 18:43:44 -0400

    inital commit

---
+++ pong.js
@@ -0,0 +1,65 @@
+const p1Button = document.querySelector("#p1Button");
+const p2Button = document.querySelector("#p2Button");
+const playTo = document.querySelector("#playto");
+const resetButton = document.querySelector("#reset");
+const playingToText = document.querySelector("label[for='playto']");
+
+const p1ScoreText = document.querySelector("#p1Display");
+const p2ScoreText = document.querySelector("#p2Display");
+
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = parseInt(playTo.value);
+// let playingToText.innerText = `Play to: ${playTo.value}`;
+
+p1Button.classList.toggle('enable-p1btn')
+p2Button.classList.toggle('enable-p2btn')
+
+p1Button.addEventListener("click", function () {
+    p1Score++;
+    p1ScoreText.innerText = p1Score;
+
+    if (p1Score == winningScore) {
+        endGame()
+    }
+});
+
+p2Button.addEventListener("click", function () {
+    p2Score++;
+    p2ScoreText.innerText = p2Score;
+
+    if (p2Score == winningScore) {
+        endGame()
+    }
+});
+
+playTo.addEventListener("change", function () {
+    winningScore = playTo.value;
+    resetScore()
+});
+
+resetButton.addEventListener("click", resetScore);
+
+function endGame() {
+    p1Button.classList.add('disable-btn')
+    p2Button.classList.add('disable-btn')
+    p1Button.disabled = true
+    p2Button.disabled = true
+}
+
+function resetScore() {
+    if (p1Score == 0 & p2Score == 0){
+        console.log("already reset")
+    } else {
+        p1Score = 0;
+        p2Score = 0;
+        p1ScoreText.innerText = p1Score;
+        p2ScoreText.innerText = p2Score;
+        p1Button.disabled = false
+        p2Button.disabled = false
+        p1Button.classList.remove('disable-btn')
+        p2Button.classList.remove('disable-btn')
+        p1Button.classList.add('enable-p1btn')
+        p2Button.classList.add('enable-p2btn')
+    }
+}
