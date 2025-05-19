1a02f519d20b8d2b714e23be8232d3cf00cc8b76
Amir Noorafkan <amir@BigMac.local>
2025-05-19 13:06:02 -0400

    add confetti animation, and display winner at the top

--- pong.js
+++ pong.js
@@ -3,9 +3,13 @@ const p2Button = document.querySelector("#p2Button");
 const playTo = document.querySelector("#playto");
 const resetButton = document.querySelector("#reset");
 const playingToText = document.querySelector("label[for='playto']");
+const winner = document.querySelector("#winner");
 
 const p1ScoreText = document.querySelector("#p1Display");
 const p2ScoreText = document.querySelector("#p2Display");
+
+const canvas = document.querySelector("#confetti")
+const jsConfetti = new JSConfetti()
 
 let p1Score = 0;
 let p2Score = 0;
@@ -20,7 +24,7 @@ p1Button.addEventListener("click", function () {
     p1ScoreText.innerText = p1Score;
 
     if (p1Score == winningScore) {
-        endGame()
+        endGame("Player 1")
     }
 });
 
@@ -29,7 +33,7 @@ p2Button.addEventListener("click", function () {
     p2ScoreText.innerText = p2Score;
 
     if (p2Score == winningScore) {
-        endGame()
+        endGame("Player 2")
     }
 });
 
@@ -40,7 +44,16 @@ playTo.addEventListener("change", function () {
 
 resetButton.addEventListener("click", resetScore);
 
-function endGame() {
+function endGame(player) {
+    jsConfetti.addConfetti({
+   emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+}).then(() => {
+    // change the text of the winner element
+        winner.innerText = player + " wins!";
+        winner.classList.add('winner')
+    }
+    )
+
     p1Button.classList.add('disable-btn')
     p2Button.classList.add('disable-btn')
     p1Button.disabled = true
@@ -51,6 +64,8 @@ function resetScore() {
     if (p1Score == 0 & p2Score == 0){
         console.log("already reset")
     } else {
+        winner.innerText = "";
+        winner.classList.remove('winner')
         p1Score = 0;
         p2Score = 0;
         p1ScoreText.innerText = p1Score;
