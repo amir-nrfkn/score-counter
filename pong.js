97632da819ed70cca05905865b1a393cd361ecf3
amir-nrfkn <amir.dev21@gmail.com>
2025-05-24 14:09:23 -0400

    refactor: created player classes, updated functions to use classes

--- pong.js
+++ pong.js
@@ -1,55 +1,45 @@
-const p1Button = document.querySelector("#p1Button");
-const p2Button = document.querySelector("#p2Button");
+const player1 = {
+    name: "Player 1",
+    button: document.querySelector("#p1Button"),
+    display: document.querySelector("#p1Display"),
+    score: 0
+}
+
+const player2 = {
+    name: "Player 2",
+    button: document.querySelector("#p2Button"),
+    display: document.querySelector("#p2Display"),
+    score: 0
+}
+
+
 const playTo = document.querySelector("#playto");
 const resetButton = document.querySelector("#reset");
 const playingToText = document.querySelector("label[for='playto']");
 const winner = document.querySelector("#winner");
 
-const p1ScoreText = document.querySelector("#p1Display");
-const p2ScoreText = document.querySelector("#p2Display");
 
 const canvas = document.querySelector("#confetti")
 const jsConfetti = new JSConfetti()
 
-let p1Score = 0;
-let p2Score = 0;
 let winningScore = parseInt(playTo.value);
 // let playingToText.innerText = `Play to: ${playTo.value}`;
 
-p1Button.classList.toggle('enable-p1btn')
-p2Button.classList.toggle('enable-p2btn')
+// p1Button.classList.toggle('enable-p1btn')
+// p2Button.classList.toggle('enable-p2btn')
 
-p1Button.addEventListener("click", function () {
-    p1Score++;
-    p1ScoreText.innerText = p1Score;
-    
-    if (p1Score == winningScore) {
-        p1ScoreText.classList.add('has-text-success', 'winner')
-        p2ScoreText.classList.add('has-text-danger', 'loser')
-        endGame("Player 1")
+function updateScore(player, opponent) {
+    player.score++;
+    player.display.innerText = player.score;
+    if (player.score === winningScore) {
+        player.display.classList.add('has-text-success')
+        opponent.display.classList.add('has-text-danger')
+        endGame(player);
     }
-});
-
-p2Button.addEventListener("click", function () {
-    p2Score++;
-    p2ScoreText.innerText = p2Score;
-    
-    if (p2Score == winningScore) {
-        p2ScoreText.classList.add('has-text-success', 'winner')
-        p1ScoreText.classList.add('has-text-danger', 'loser')
-        endGame("Player 2")
-    }
-});
-
-playTo.addEventListener("change", function () {
-    winningScore = playTo.value;
-    resetScore()
-});
-
-resetButton.addEventListener("click", resetScore);
+}
 
 function endGame(player) {
-    winner.innerText = player + " wins!";
+    winner.innerText = player.name + " wins!";
     winner.classList.add('has-text-success', 'winner')
 
     jsConfetti.addConfetti({
@@ -61,23 +51,29 @@ function endGame(player) {
 }
 
 function resetScore() {
-    if (p1Score == 0 & p2Score == 0){
-        console.log("already reset")
-    } else {
+
+    for (let p of [player1, player2]) {
+        p.score = 0;
+        p.display.innerText = p.score;
+        p.display.classList.remove('has-text-success', 'has-text-danger')
+        p.button.disabled = false
         winner.innerText = "Ping Pong Score Keeper";
         winner.classList.remove('has-text-success', 'winner')
-
-        p2ScoreText.classList.remove('has-text-success')
-        p1ScoreText.classList.remove('has-text-success')
-        p1ScoreText.classList.remove('has-text-danger', 'loser')
-        p2ScoreText.classList.remove('has-text-danger', 'loser')
-
-        p1Score = 0;
-        p2Score = 0;
-        p1ScoreText.innerText = p1Score;
-        p2ScoreText.innerText = p2Score;
-        p1Button.disabled = false
-        p2Button.disabled = false
-
     }
 }
+
+
+player1.button.addEventListener("click", function () {
+    updateScore(player1, player2);
+});
+
+player2.button.addEventListener("click", function () {
+    updateScore(player2, player1);
+});
+
+playTo.addEventListener("change", function () {
+    winningScore = playTo.value;
+    resetScore()
+});
+
+resetButton.addEventListener("click", resetScore);
